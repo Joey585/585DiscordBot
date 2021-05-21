cf081e9063f05ee93b8e7e7025f0c2e28f3e7f4b
Joey585 <thelich06@protonmail.com>
2021-05-21 11:00:29 -0700

    Renamed index.js
    v0.022

---
+++ 585.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+require('discord-reply');
+const client = new Discord.Client();
+const {token} = require('./config.json');
+const {mongodbSRV} = require('./config.json');
+const mongoose = require('mongoose');
+
+client.on('ready', () => {
+    client.user.setPresence({activity: {name: `Ping Me!`}, status: 'dnd'})
+})
+
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+
+['command_handler', 'event_handler'].forEach(handler => {
+    require(`./handlers/${handler}`)(client, Discord);
+})
+
+mongoose.connect(`${mongodbSRV}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log(`Connected to the database.`)
+}).catch((err) => {
+    console.log(err);
+})
+
+client.login(token)
